fec44a042196ffb0ee012d98740a678c610e40ed
Kondo Takeo <takeoverjp@gmail.com>
2020-11-25 22:58:11 +0900

    Add 104.

---
+++ 104.maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=104 lang=cpp
+ *
+ * [104] Maximum Depth of Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (!root) return 0;
+        return std::max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    }
+};
+// @lc code=end
+
