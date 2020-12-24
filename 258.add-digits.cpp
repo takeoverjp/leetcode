6db930349098cab31261dce7389f730dddd4efb1
Kondo Takeo <takeoverjp@gmail.com>
2020-12-25 00:11:57 +0900

    Improve 258 with congruence formula.

--- 258.add-digits.cpp
+++ 258.add-digits.cpp
@@ -5,30 +5,34 @@
  */
 
 #include <bits/stdc++.h>
+
 #include <cassert>
 
 using namespace std;
 
 // @lc code=start
 class Solution {
-public:
-    int addDigits(int num) {
-        while (num >= 10) {
-            int val = num;
-            int sum = 0;
-            while (val) {
-                sum += val % 10;
-                val /= 10;
-            }
-            num = sum;
-        }
-        return num;
+ public:
+  int addDigits(int num) {
+    if (!num) return num;
+    return (num % 9) ? (num % 9) : 9;
+  }
+  int addDigitsNormal(int num) {
+    while (num >= 10) {
+      int val = num;
+      int sum = 0;
+      while (val) {
+        sum += val % 10;
+        val /= 10;
+      }
+      num = sum;
     }
+    return num;
+  }
 };
 // @lc code=end
 
-int
-main () {
+int main() {
   Solution s;
   assert(s.addDigits(10) == 1);
   assert(s.addDigits(38) == 2);
