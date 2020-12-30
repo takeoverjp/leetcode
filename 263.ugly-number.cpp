db0103a859e112ed0a44141bd50fd0585219dc48
Kondo Takeo <takeoverjp@gmail.com>
2020-12-30 23:00:49 +0900

    Add 263.

---
+++ 263.ugly-number.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=263 lang=cpp
+ *
+ * [263] Ugly Number
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isUgly(int num) {
+        
+    }
+};
+// @lc code=end
+
