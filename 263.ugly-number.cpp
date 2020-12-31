6f02235f95ac1de8defb1e33c124f54c9100a1f1
Kondo Takeo <takeoverjp@gmail.com>
2020-12-31 20:42:30 +0900

    Add 263.

--- 263.ugly-number.cpp
+++ 263.ugly-number.cpp
@@ -6,10 +6,23 @@
 
 // @lc code=start
 class Solution {
-public:
-    bool isUgly(int num) {
-        
+ public:
+  bool isUgly(int num) {
+    if (num == 0) {
+      return false;
     }
+    if (num == 1) {
+      return true;
+    }
+    if (num % 2 == 0) {
+      return isUgly(num / 2);
+    } else if (num % 3 == 0) {
+      return isUgly(num / 3);
+    } else if (num % 5 == 0) {
+      return isUgly(num / 5);
+    } else {
+      return false;
+    }
+  }
 };
 // @lc code=end
-
