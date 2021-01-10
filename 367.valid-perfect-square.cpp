f1eca9338929662d7fb016aff84981268daf0fc0
Kondo Takeo <takeoverjp@gmail.com>
2021-01-10 21:06:52 +0900

    Impl 367 without stdlib.

--- 367.valid-perfect-square.cpp
+++ 367.valid-perfect-square.cpp
@@ -13,13 +13,37 @@ using namespace std;
 // @lc code=start
 class Solution {
  public:
-  bool isPerfectSquare(int num) { return (int)pow((int)sqrt(num), 2) == num; }
+  bool isPerfectSquare2(int num) { return (int)pow((int)sqrt(num), 2) == num; }
+  bool isPerfectSquare(int num) {
+    if (num == 1) {
+      return true;
+    }
+    int start = 1;
+    int end = num;
+    while (start <= end) {
+      int64_t candidate = ((int64_t)start + (int64_t)end) / 2;
+      int64_t sqr = candidate * candidate;
+      if (sqr == num) {
+        return true;
+      }
+      if (sqr > num) {
+        end = candidate - 1;
+        candidate = (start + candidate) / 2;
+      } else {
+        start = candidate + 1;
+        candidate = (end + candidate) / 2;
+      }
+    }
+    return false;
+  }
 };
 // @lc code=end
 
 int main() {
   Solution s;
-  assert(s.isPerfectSquare(0));
+  assert(s.isPerfectSquare(64516));
+  assert(s.isPerfectSquare(2000105819) == false);
+  assert(s.isPerfectSquare(9));
   assert(s.isPerfectSquare(15) == false);
   assert(s.isPerfectSquare(16));
   assert(s.isPerfectSquare(17) == false);
