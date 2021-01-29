9a6588f2dcb43e5dd61651e9d00701ddb240101b
Kondo Takeo <takeoverjp@gmail.com>
2021-01-29 12:04:47 +0900

    459. Repeated Substring Pattern.
    Change-Id: Ida733ee225e42bc746d6caf8b203f29c7414428e

---
+++ 459.repeated-substring-pattern.cpp
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode id=459 lang=cpp
+ *
+ * [459] Repeated Substring Pattern
+ */
+
+#include <bits/stdc++.h>
+#include <cassert>
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+ public:
+  bool repeatedSubstringPattern(string s) {
+    char first = s[0];
+    int len = s.length();
+    if (len <= 1) {
+      return false;
+    }
+    for (int i = 1; i < len; i++) {
+      if (s[i] == first) {
+        int sublen = i;
+        if (len % sublen) {
+          continue;
+        }
+ 
+        string pattern = s.substr(0, sublen);
+        for (int j = sublen; j < len; j += sublen) {
+
+          // cout << pattern << " ? " << s.substr(j, sublen) << endl;
+          if (s.substr(j, sublen) != pattern) {
+            goto NEXT_SUBLEN;
+          }
+        }
+
+        return true;
+
+      NEXT_SUBLEN:
+        continue;
+      }
+    }
+    return false;
+  }
+};
+// @lc code=end
+
+int main() {
+  Solution s;
+  assert(s.repeatedSubstringPattern("a") == false);
+  assert(s.repeatedSubstringPattern("aa") == true);
+  assert(s.repeatedSubstringPattern("abab") == true);
+  assert(s.repeatedSubstringPattern("ababab") == true);
+  assert(s.repeatedSubstringPattern("abaabaaba") == true);
+  return 0;
+}
