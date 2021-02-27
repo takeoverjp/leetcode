4ec7469d8432300ea2db04305a9063bede235b11
Kondo Takeo <takeoverjp@gmail.com>
2021-02-27 23:31:16 +0900

    589. N-ary Tree Preorder Traversal

---
+++ 589.n-ary-tree-preorder-traversal.cpp
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode id=589 lang=cpp
+ *
+ * [589] N-ary Tree Preorder Traversal
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> preorder(Node* root) {
+        vector<int> ret;
+        preorder(root, ret);
+        return ret;
+    }
+    void preorder(Node* root, vector<int>& ret) {
+        if (root == nullptr) {
+            return;
+        }
+        ret.push_back(root->val);
+        for (auto child : root->children) {
+            preorder(child, ret);
+        }
+    }
+};
+// @lc code=end
+
