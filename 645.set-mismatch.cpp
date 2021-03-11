80965c323d54cc6d3f3667e57e88da837338ef47
Kondo Takeo <takeoverjp@gmail.com>
2021-03-11 12:41:36 +0900

    Imporve 645.
    Change-Id: Ibe4228754b8bd085baaa684c5450eacea5a9bac9

--- 645.set-mismatch.cpp
+++ 645.set-mismatch.cpp
@@ -14,20 +14,22 @@ class Solution {
  public:
   vector<int> findErrorNums(vector<int>& nums) {
     vector<int> ret;
-    map<int, int> result;
-    for (const auto num : nums) {
-      if (result[num]) {
-        ret.push_back(num);
-      }
-      result[num] = true;
-    }
     int size = nums.size();
-    for (int i = 1; i <= size; i++) {
-      if (result[i] == 0) {
-        ret.push_back(i);
-        return ret;
+    for (int i = 0; i < size; i++) {
+      if (nums[abs(nums[i]) - 1] < 0) {
+        ret.push_back(abs(nums[i]));
+      } else {
+        nums[abs(nums[i]) - 1] *= -1;
       }
     }
+    for (int i = 0; i < size; i++) {
+      // cout << "" << i << ", " << nums[i] << endl;
+      if (nums[i] > 0) {
+        ret.push_back(i + 1);
+        break;
+      }
+    }
+
     return ret;
   }
 };
@@ -36,13 +38,19 @@ class Solution {
 int main() {
   Solution s;
   vector<int> nums, exp;
-  nums = {1, 2, 2, 4};
+  nums = {1, 2, 2, 3, 4};
+  exp = {2, 5};
+  assert(s.findErrorNums(nums) == exp);
+  nums = {1, 2, 2};
   exp = {2, 3};
+  assert(s.findErrorNums(nums) == exp);
+  nums = {1, 3, 3, 4};
+  exp = {3, 2};
   assert(s.findErrorNums(nums) == exp);
   nums = {1, 1};
   exp = {1, 2};
   assert(s.findErrorNums(nums) == exp);
-  nums = {1, 2, 2, 4};
+  nums = {1, 4, 2, 5, 2};
   exp = {2, 3};
   assert(s.findErrorNums(nums) == exp);
   return 0;
