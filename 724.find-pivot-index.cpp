3d91538ff51d020ed11dd7b820470c0bbc6899bb
Kondo Takeo <takeoverjp@gmail.com>
2021-04-06 13:11:38 +0900

    724. Find Pivot Index
    Change-Id: Ia64fbda4cf5b714b73dac23fac497108c51ccc89

---
+++ 724.find-pivot-index.cpp
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode id=724 lang=cpp
+ *
+ * [724] Find Pivot Index
+ */
+
+#include <bits/stdc++.h>
+#include "lassert/lassert.h"
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+ public:
+  int pivotIndex(vector<int>& nums) {
+    int left = 0;
+    int right = 0;
+    int size = nums.size();
+    for (int i = 1; i < size; i++) {
+      right += nums[i];
+    }
+    if (left == right) {
+      return 0;
+    }
+    for (int i = 1; i < size; i++) {
+      left += nums[i - 1];
+      right -= nums[i];
+      if (left == right) {
+        return i;
+      }
+    }
+    return -1;
+  }
+};
+// @lc code=end
+
+int main() {
+  Solution s;
+  vector<int> nums;
+  nums = {1};
+  ASSERT_EQ(s.pivotIndex(nums), 0);
+  nums = {1, 7, 3, 6, 5, 6};
+  ASSERT_EQ(s.pivotIndex(nums), 3);
+  nums = {1, 2, 3};
+  ASSERT_EQ(s.pivotIndex(nums), -1);
+  nums = {2, 1, -1};
+  ASSERT_EQ(s.pivotIndex(nums), 0);
+  return 0;
+}
