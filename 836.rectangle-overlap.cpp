3680525cd149e4f96cf21b3c9330f8eb167c0e66
Kondo Takeo <takeoverjp@gmail.com>
2021-04-28 12:41:14 +0900

    836. Rectangle Overlap
    Change-Id: I00b0d5a1c4821b616a8afcdf2ccecd8f070bb3dd

--- 836.rectangle-overlap.cpp
+++ 836.rectangle-overlap.cpp
@@ -15,15 +15,19 @@ class Solution {
   bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
     int w1 = rec1[2] - rec1[0];
     int h1 = rec1[3] - rec1[1];
-    int w2 = rec2[2] - rec1[0];
+    int w2 = rec2[2] - rec2[0];
     int h2 = rec2[3] - rec2[1];
     if (!w1 || !h1 || !w2 || !h2) {
       return false;
     }
     if (((rec1[0] < rec2[0] && rec2[0] < rec1[2]) ||
-         (rec2[0] < rec1[0] && rec1[0] < rec2[2])) &&
+         (rec2[0] < rec1[0] && rec1[0] < rec2[2]) ||
+         (rec1[0] < rec2[2] && rec2[2] < rec1[2]) ||
+         (rec2[0] < rec1[2] && rec1[2] < rec2[2])) &&
         ((rec1[1] < rec2[1] && rec2[1] < rec1[3]) ||
-         (rec2[1] < rec1[1] && rec1[1] < rec2[3]))) {
+         (rec2[1] < rec1[1] && rec1[1] < rec2[3]) ||
+         (rec1[1] < rec2[3] && rec2[3] < rec1[3]) ||
+         (rec2[1] < rec1[3] && rec1[3] < rec2[3]))) {
       return true;
     }
     return false;
@@ -52,5 +56,8 @@ int main() {
   rec1 = {7, 8, 13, 15};
   rec2 = {10, 8, 12, 20};
   ASSERT_EQ(s.isRectangleOverlap(rec1, rec2), true);
+  rec1 = {-1, 0, 1, 1};
+  rec2 = {0, -1, 0, 1};
+  ASSERT_EQ(s.isRectangleOverlap(rec1, rec2), false);
   return 0;
 }
