6371587daae2267a1498737bdc6c5f2dd8656200
Kondo Takeo <takeoverjp@gmail.com>
2021-04-30 20:06:16 +0900

    Impl 852 with O(logN)
    Change-Id: I60c4f8ba81bb34a3c559025f6701281f7de89b3d

--- 852.peak-index-in-a-mountain-array.cpp
+++ 852.peak-index-in-a-mountain-array.cpp
@@ -14,9 +14,22 @@ class Solution {
  public:
   int peakIndexInMountainArray(vector<int>& arr) {
     int size = arr.size();
-    for (int i = 1; i < size; i++) {
-      if (arr[i - 1] > arr[i]) {
-        return i - 1;
+    int start = 0;
+    int end = size - 1;
+    int mid = (start + end) / 2;
+    while (true) {
+      if ((arr[mid - 1] < arr[mid]) && (arr[mid] > arr[mid + 1])) {
+        return mid;
+      }
+      if (arr[mid - 1] < arr[mid]) {
+          start = min(end, mid + 1);
+          mid = min(size - 2, (start + end) / 2);
+          continue;
+      }
+      if (arr[mid - 1] > arr[mid]) {
+          end = max(start, mid - 1);
+          mid = max(1, (start + end) / 2);
+          continue;
       }
     }
     return -1;
@@ -35,6 +48,8 @@ int main() {
   ASSERT_EQ(s.peakIndexInMountainArray(arr), 1);
   arr = {3, 4, 5, 1};
   ASSERT_EQ(s.peakIndexInMountainArray(arr), 2);
+  arr = {3, 5, 3, 2, 0};
+  ASSERT_EQ(s.peakIndexInMountainArray(arr), 1);
   arr = {24, 69, 100, 99, 79, 78, 67, 36, 26, 19};
   ASSERT_EQ(s.peakIndexInMountainArray(arr), 2);
   return 0;
