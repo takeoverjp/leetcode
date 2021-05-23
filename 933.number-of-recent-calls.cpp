0b6dd027e24d6860f6187be1e1d9ad6f5a44a96a
Kondo Takeo <takeoverjp@gmail.com>
2021-05-23 21:43:29 +0900

    933. Number of Recent Calls

---
+++ 933.number-of-recent-calls.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=933 lang=cpp
+ *
+ * [933] Number of Recent Calls
+ */
+
+// @lc code=start
+class RecentCounter {
+ public:
+  RecentCounter() {}
+
+  int ping(int t) {
+    requests_.push_back(t);
+    for (auto it = requests_.begin(); it != requests_.end();) {
+      if (*it < t - 3000) {
+        it = requests_.erase(it);
+      } else {
+        it++;
+      }
+    }
+    return requests_.size();
+  }
+
+ private:
+  std::vector<int> requests_;
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
+// @lc code=end
