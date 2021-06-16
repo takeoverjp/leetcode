faf3d4a81ff1403d6efe640ed9580fc43075c136
Kondo Takeo <takeoverjp@gmail.com>
2021-06-16 19:47:33 +0900

    8. String to Integer (atoi)

--- 8.string-to-integer-atoi.cpp
+++ 8.string-to-integer-atoi.cpp
@@ -13,18 +13,46 @@ using namespace std;
 class Solution {
  public:
   int myAtoi(string s) {
+    enum State {
+      kSpaceState,
+      kSignState,
+      kDigitState,
+    } state = kSpaceState;
     bool is_negative = false;
-    int ret = 0;
+    uint64_t ret = 0;
     for (const char c : s) {
-      if (isspace(c)) {
-        continue;
+      if (state == kSpaceState) {
+        if (isspace(c)) {
+          continue;
+        } else {
+          state = kSignState;
+        }
       }
-      if (c == '-') {
-        is_negative = true;
-        continue;
+      if (state == kSignState) {
+        if (c == '-') {
+          is_negative = true;
+          state = kDigitState;
+          continue;
+        } else if (c == '+') {
+          state = kDigitState;
+          continue;
+        } else {
+          state = kDigitState;
+        }
       }
-      char num = c - '0';
-      ret = ret * 10 + num;
+      if (state == kDigitState) {
+        if (!isdigit(c)) {
+          break;
+        }
+        char num = c - '0';
+        ret = ret * 10 + num;
+        if (is_negative && ret > 2147483648) {
+          return -2147483648;
+        }
+        if (!is_negative && ret > 2147483647) {
+          return 2147483647;
+        }
+      }
     }
     return is_negative ? (-1 * ret) : ret;
   }
@@ -36,5 +64,10 @@ int main() {
   ASSERT_EQ(s.myAtoi("42"), 42);
   ASSERT_EQ(s.myAtoi("3"), 3);
   ASSERT_EQ(s.myAtoi("   -42"), -42);
+  ASSERT_EQ(s.myAtoi("4193 with words"), 4193);
+  ASSERT_EQ(s.myAtoi("words and 987"), 0);
+  ASSERT_EQ(s.myAtoi("-91283472332"), -2147483648);
+  ASSERT_EQ(s.myAtoi("+1"), 1);
+  ASSERT_EQ(s.myAtoi("+-12"), 0);
   return 0;
 }
