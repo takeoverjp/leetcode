873009ab490899a6298f67d16461caa3d51ea879
Kondo Takeo <takeoverjp@gmail.com>
2021-06-16 20:25:22 +0900

    Refactor 8

--- 8.string-to-integer-atoi.cpp
+++ 8.string-to-integer-atoi.cpp
@@ -13,44 +13,28 @@ using namespace std;
 class Solution {
  public:
   int myAtoi(string s) {
-    enum State {
-      kSpaceState,
-      kSignState,
-      kDigitState,
-    } state = kSpaceState;
     bool is_negative = false;
-    uint64_t ret = 0;
-    for (const char c : s) {
-      if (state == kSpaceState) {
-        if (isspace(c)) {
-          continue;
-        } else {
-          state = kSignState;
+    int64_t ret = 0;
+    int i = 0;
+    while (s[i] == ' ') {
+      i++;
+    }
+    if (s[i] == '+') {
+      i++;
+    } else if (s[i] == '-') {
+      is_negative = true;
+      i++;
+    }
+    while (isdigit(s[i])) {
+      char num = s[i++] - '0';
+      ret = ret * 10 + num;
+      if (is_negative) {
+        if (ret - 1 > INT32_MAX) {
+          return INT32_MIN;
         }
-      }
-      if (state == kSignState) {
-        if (c == '-') {
-          is_negative = true;
-          state = kDigitState;
-          continue;
-        } else if (c == '+') {
-          state = kDigitState;
-          continue;
-        } else {
-          state = kDigitState;
-        }
-      }
-      if (state == kDigitState) {
-        if (!isdigit(c)) {
-          break;
-        }
-        char num = c - '0';
-        ret = ret * 10 + num;
-        if (is_negative && ret > 2147483648) {
-          return -2147483648;
-        }
-        if (!is_negative && ret > 2147483647) {
-          return 2147483647;
+      } else {
+        if (ret > INT32_MAX) {
+          return INT32_MAX;
         }
       }
     }
@@ -66,7 +50,8 @@ int main() {
   ASSERT_EQ(s.myAtoi("   -42"), -42);
   ASSERT_EQ(s.myAtoi("4193 with words"), 4193);
   ASSERT_EQ(s.myAtoi("words and 987"), 0);
-  ASSERT_EQ(s.myAtoi("-91283472332"), -2147483648);
+  ASSERT_EQ(s.myAtoi("-91283472332"), INT32_MIN);
+  ASSERT_EQ(s.myAtoi("-2147483648"), -2147483648);
   ASSERT_EQ(s.myAtoi("+1"), 1);
   ASSERT_EQ(s.myAtoi("+-12"), 0);
   return 0;
