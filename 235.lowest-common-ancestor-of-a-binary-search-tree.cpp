eeb0f3d8a961341d5542f11960ed4d4d880170d4
Kondo Takeo <takeoverjp@gmail.com>
2021-07-11 22:48:25 +0900

    Improve 235

--- 235.lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235.lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -30,45 +30,13 @@ struct TreeNode {
 class Solution {
  public:
   TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-    vector<TreeNode*> ancestor_p;
-    vector<TreeNode*> ancestor_q;
-    findNode(root, p, ancestor_p);
-    findNode(root, q, ancestor_q);
-    int size_p = ancestor_p.size();
-    int size_q = ancestor_q.size();
-    int size = std::min(size_p, size_q);
-    int ret = 0;
-    while (ret < size) {
-    //   cout << "p = " << ancestor_p[size_p - ret - 1]->val << std::endl;
-    //   cout << "q = " << ancestor_q[size_q - ret - 1]->val << std::endl;
-      if (ancestor_p[size_p - ret - 1]->val !=
-          ancestor_q[size_q - ret - 1]->val) {
-        break;
-      }
-      ret++;
+    if (root->val < p->val && root->val < q->val) {
+      return lowestCommonAncestor(root->right, p, q);
     }
-    return ancestor_p[size_p - ret];
-  }
-
- private:
-  bool findNode(TreeNode* root, TreeNode* p, vector<TreeNode*>& ancestor) {
-    if (!root) {
-      return false;
+    if (root->val > p->val && root->val > q->val) {
+      return lowestCommonAncestor(root->left, p, q);
     }
-    if (root == p) {
-      ancestor.push_back(root);
-      return true;
-    }
-    bool ret = findNode(root->left, p, ancestor);
-    if (ret) {
-      ancestor.push_back(root);
-      return ret;
-    }
-    ret = findNode(root->right, p, ancestor);
-    if (ret) {
-      ancestor.push_back(root);
-    }
-    return ret;
+    return root;
   }
 };
 // @lc code=end
