e3692da544b74d880ccb0871c4044d3bf96ff34b
Kondo Takeo <takeoverjp@gmail.com>
2021-07-17 23:51:06 +0900

    [495] Teemo Attacking

---
+++ 495.teemo-attacking.cpp
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode id=495 lang=cpp
+ *
+ * [495] Teemo Attacking
+ */
+
+#include <bits/stdc++.h>
+#include "lassert/lassert.h"
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+ public:
+  int findPoisonedDuration(vector<int>& timeSeries, int duration) {
+    int size = timeSeries.size();
+    int ret = 0;
+    for (int i = 0; i < size; i++) {
+      if (i == size - 1) {
+        ret += duration;
+        break;
+      }
+
+      ret += std::min(timeSeries[i + 1] - timeSeries[i], duration);
+    }
+    return ret;
+  }
+};
+// @lc code=end
+
+int main() {
+  Solution s;
+  vector<int> timeSeries;
+  timeSeries = {0};
+  ASSERT_EQ(s.findPoisonedDuration(timeSeries, 0), 0);
+  timeSeries = {1, 4};
+  ASSERT_EQ(s.findPoisonedDuration(timeSeries, 2), 4);
+  timeSeries = {1, 2};
+  ASSERT_EQ(s.findPoisonedDuration(timeSeries, 2), 3);
+  timeSeries = {1, 1, 4};
+  ASSERT_EQ(s.findPoisonedDuration(timeSeries, 2), 4);
+  return 0;
+}
