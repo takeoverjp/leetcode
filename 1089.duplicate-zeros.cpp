869927c9d1f2f5e799f4600cf5889c47cb0406ce
Kondo Takeo <takeoverjp@gmail.com>
2021-07-22 20:37:30 +0900

    Improve 1089.

--- 1089.duplicate-zeros.cpp
+++ 1089.duplicate-zeros.cpp
@@ -14,19 +14,23 @@ class Solution {
  public:
   void duplicateZeros(vector<int>& arr) {
     size_t size = arr.size();
-    for (int i = 0; i < size; i++) {
-      if (arr[i] != 0) {
-        continue;
+    int zero_num = std::count(arr.begin(), arr.end(), 0);
+    int new_size = size + zero_num;
+    for (int org_idx = size - 1, new_idx = new_size - 1;
+         org_idx >= 0 && new_idx >= 0; org_idx--, new_idx--) {
+      if (arr[org_idx] != 0) {
+        if (new_idx < size) {
+          arr[new_idx] = arr[org_idx];
+        }
+      } else {
+        if (new_idx < size) {
+          arr[new_idx] = 0;
+        }
+        new_idx--;
+        if (new_idx < size) {
+          arr[new_idx] = 0;
+        }
       }
-
-      if (i == size - 1) {
-        break;
-      }
-      for (int j = size - 1; j > i + 1; j--) {
-        arr[j] = arr[j - 1];
-      }
-      arr[i + 1] = 0;
-      i++;
     }
   }
 };
