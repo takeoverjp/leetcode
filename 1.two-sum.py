478330efd63f614f22c49c0d4126bf6ad686c434
Kondo Takeo <takeoverjp@gmail.com>
2021-08-13 18:51:46 +0900

    [py3][1] improve performance
    Change-Id: Icdc6f893a0caf92e049636c25d9de55e7c57b91b

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -11,10 +11,12 @@ from typing import List
 # @lc code=start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(0, len(nums) - 1):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        dict = {}
+        for i, n in enumerate(nums):
+            m = target - n
+            if m in dict:
+                return [dict[m], i]
+            dict[n] = i
 
 # @lc code=end
 
