ecaa7146ad61162707603f53068549e47a1a8859
Kondo Takeo <takeoverjp@gmail.com>
2021-08-21 19:03:22 +0900

    [c++][1260] Shift 2D Grid
    Change-Id: I351e220c755789390b681b5a8167ccfda44e7474

---
+++ 1260.shift-2-d-grid.cpp
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode id=1260 lang=cpp
+ *
+ * [1260] Shift 2D Grid
+ */
+
+#include <bits/stdc++.h>
+#include "lassert/lassert.h"
+
+using namespace std;
+
+#include <bits/stdc++.h>
+#include "lassert/lassert.h"
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+ public:
+  vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+    int m = grid.size();
+    int n = grid[0].size();
+    int num = m * n;
+    k = k % num;
+    auto ret = grid;
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        int new_j = (j + k) % n;
+        int new_i = (i + (j + k) / m) % m;
+        ret[new_i][new_j] = grid[i][j];
+      }
+    }
+    return ret;
+  }
+};
+// @lc code=end
+
+int main() {
+  Solution s;
+  vector<vector<int>> grid, exp;
+
+  grid = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+  exp = {{9, 1, 2}, {3, 4, 5}, {6, 7, 8}};
+  ASSERT_EQ(s.shiftGrid(grid, 1), exp);
+
+  grid = {{3, 8, 1, 9}, {19, 7, 2, 5}, {4, 6, 11, 10}, {12, 0, 21, 13}};
+  exp = {{12, 0, 21, 13}, {3, 8, 1, 9}, {19, 7, 2, 5}, {4, 6, 11, 10}};
+  ASSERT_EQ(s.shiftGrid(grid, 4), exp);
+
+  grid = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+  exp = grid;
+  ASSERT_EQ(s.shiftGrid(grid, 9), exp);
+  return 0;
+}
