6c794f90cdad6dbabcd02acd4ac56395fb22cc23
Kondo Takeo <takeoverjp@gmail.com>
2021-08-28 21:41:48 +0900

    [c++][1309] Decrypt String from Alphabet to Integer Mapping
    Change-Id: Ied2cabb55ac8c206f408e4d0dcead9e46041608d

---
+++ 1309.decrypt-string-from-alphabet-to-integer-mapping.cpp
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=1309 lang=cpp
+ *
+ * [1309] Decrypt String from Alphabet to Integer Mapping
+ */
+
+#include <bits/stdc++.h>
+#include "lassert/lassert.h"
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+ public:
+  string freqAlphabets(string s) {
+    int size = s.length();
+    std::string ret;
+    for (int i = size - 1; i >= 0; i--) {
+      char c = 'a' - 1;
+      if (s[i] == '#') {
+        c += (s[i - 2] - '0') * 10 + (s[i - 1] - '0');
+        i -= 2;
+      } else {
+        c += s[i] - '0';
+      }
+      ret = c + ret;
+    }
+    return ret;
+  }
+};
+// @lc code=end
+
+int main() {
+  Solution s;
+  ASSERT_EQ(s.freqAlphabets("10#11#12"), "jkab");
+  ASSERT_EQ(s.freqAlphabets("1326#"), "acz");
+  ASSERT_EQ(s.freqAlphabets("25#"), "y");
+  ASSERT_EQ(s.freqAlphabets(
+                "12345678910#11#12#13#14#15#16#17#18#19#20#21#22#23#24#25#26#"),
+            "abcdefghijklmnopqrstuvwxyz");
+  return 0;
+}
