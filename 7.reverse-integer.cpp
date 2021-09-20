b6d2b0b0de450e90ef32949fa00433f50c8f4a17
Kondo Takeo <takeoverjp@gmail.com>
2021-09-20 23:32:28 +0900

    [c++][7] add test code

--- 7.reverse-integer.cpp
+++ 7.reverse-integer.cpp
@@ -4,20 +4,30 @@
  * [7] Reverse Integer
  */
 
-#include <vector>
-#include <cstdint>
+#include <bits/stdc++.h>
+#include "lassert/lassert.h"
+
+using namespace std;
 
 // @lc code=start
 class Solution {
-public:
-    int reverse(int x) {
-        long y = 0;
-        while (x) {
-            y = y * 10 + x % 10;
-            x /= 10;
-        }
-        return (y >= INT32_MAX || y <= INT32_MIN) ? 0 : y;
+ public:
+  int reverse(int x) {
+    long y = 0;
+    while (x) {
+      y = y * 10 + x % 10;
+      x /= 10;
     }
+    return (y >= INT32_MAX || y <= INT32_MIN) ? 0 : y;
+  }
 };
 // @lc code=end
 
+int main() {
+  Solution s;
+  ASSERT_EQ(s.reverse(123), 321);
+  ASSERT_EQ(s.reverse(-123), -321);
+  ASSERT_EQ(s.reverse(120), 21);
+  ASSERT_EQ(s.reverse(0), 0);
+  return 0;
+}
