fcf37b9d5b4878751c70ab262b0cc339058c9e4e
Kondo Takeo <takeoverjp@gmail.com>
2021-09-26 14:01:58 +0900

    [c++][26] improve performance by skipping erase

--- 26.remove-duplicates-from-sorted-array.cpp
+++ 26.remove-duplicates-from-sorted-array.cpp
@@ -13,8 +13,7 @@ using namespace std;
 class Solution {
  public:
   int removeDuplicates(vector<int>& nums) {
-    nums.erase(unique(nums.begin(), nums.end()), nums.end());
-    return nums.size();
+    return std::unique(nums.begin(), nums.end()) - nums.begin();
   }
 };
 // @lc code=end
@@ -26,12 +25,16 @@ int main() {
   nums = {1, 1, 2};
   exp = {1, 2};
   ASSERT_EQ(s.removeDuplicates(nums), 2);
-  ASSERT_EQ(nums, exp);
+  for (int i = 0; i < 2; i++) {
+    ASSERT_EQ(nums[i], exp[i]);
+  }
 
   nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
   exp = {0, 1, 2, 3, 4};
   ASSERT_EQ(s.removeDuplicates(nums), 5);
-  ASSERT_EQ(nums, exp);
+  for (int i = 0; i < 5; i++) {
+    ASSERT_EQ(nums[i], exp[i]);
+  }
 
   return 0;
 }
