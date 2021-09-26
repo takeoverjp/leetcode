dd8e0891e0b07427535b3a02b7b2c13f378f55da
Kondo Takeo <takeoverjp@gmail.com>
2021-09-26 13:56:35 +0900

    [c++][26] add test

--- 26.remove-duplicates-from-sorted-array.cpp
+++ 26.remove-duplicates-from-sorted-array.cpp
@@ -4,6 +4,11 @@
  * [26] Remove Duplicates from Sorted Array
  */
 
+#include <bits/stdc++.h>
+#include "lassert/lassert.h"
+
+using namespace std;
+
 // @lc code=start
 class Solution {
  public:
@@ -13,3 +18,20 @@ class Solution {
   }
 };
 // @lc code=end
+
+int main() {
+  Solution s;
+  vector<int> nums, exp;
+
+  nums = {1, 1, 2};
+  exp = {1, 2};
+  ASSERT_EQ(s.removeDuplicates(nums), 2);
+  ASSERT_EQ(nums, exp);
+
+  nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+  exp = {0, 1, 2, 3, 4};
+  ASSERT_EQ(s.removeDuplicates(nums), 5);
+  ASSERT_EQ(nums, exp);
+
+  return 0;
+}
