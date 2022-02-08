77a100467482c24e7f3206b2bbe43ab6f480b861
Kondo Takeo <takeoverjp@gmail.com>
2022-02-08 22:24:07 +0900

    [c++][1688] Count of Matches in Tournament

---
+++ 1688.count-of-matches-in-tournament.cpp
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=1688 lang=cpp
+ *
+ * [1688] Count of Matches in Tournament
+ */
+
+// @lc code=start
+class Solution {
+ public:
+  int numberOfMatches(int n) { return n - 1; }
+};
+// @lc code=end
