793352c866eb959117fb9edd8a7092567c2ff3ed
Kondo Takeo <takeoverjp@gmail.com>
2022-04-09 00:06:35 +0900

    [c++][1979] Find Greatest Common Divisor of Array

--- 1979.find-greatest-common-divisor-of-array.cpp
+++ 1979.find-greatest-common-divisor-of-array.cpp
@@ -15,14 +15,29 @@ class Solution {
   int findGCD(vector<int>& nums) {
     sort(begin(nums), end(nums));
     int max_cand = nums[nums.size() - 1];
-    int min_cand = nums[0] while (true) {}
-    return 1;
+    int min_cand = nums[0];
+    while (max_cand - min_cand > 1) {
+      int a = max_cand - min_cand;
+      if (a > min_cand) {
+        max_cand = a;
+      } else {
+        max_cand = min_cand;
+        min_cand = a;
+      }
+    }
+    return (max_cand == min_cand) ? max_cand : 1;
   }
 };
 // @lc code=end
 
 int main() {
   Solution s;
-  ASSERT_EQ(s.(), );
+  vector<int> nums;
+  nums = {2, 5, 6, 9, 10};
+  ASSERT_EQ(s.findGCD(nums), 2);
+  nums = {7, 5, 6, 8, 3};
+  ASSERT_EQ(s.findGCD(nums), 1);
+  nums = {3, 3};
+  ASSERT_EQ(s.findGCD(nums), 3);
   return 0;
 }
