e56d26b594ba5552aa51f3a51a9e87d8a9a63733
Kondo Takeo <takeoverjp@gmail.com>
2022-04-08 23:59:24 +0900

    [c++][1979] Find Greatest Common Divisor of Array

---
+++ 1979.find-greatest-common-divisor-of-array.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=1979 lang=cpp
+ *
+ * [1979] Find Greatest Common Divisor of Array
+ */
+
+#include <bits/stdc++.h>
+#include "lassert/lassert.h"
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+ public:
+  int findGCD(vector<int>& nums) {
+    sort(begin(nums), end(nums));
+    int max_cand = nums[nums.size() - 1];
+    int min_cand = nums[0] while (true) {}
+    return 1;
+  }
+};
+// @lc code=end
+
+int main() {
+  Solution s;
+  ASSERT_EQ(s.(), );
+  return 0;
+}
