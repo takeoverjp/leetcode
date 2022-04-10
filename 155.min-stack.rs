1f968ffb87f3234976f10468b89671e431ba3fe9
Kondo Takeo <takeoverjp@gmail.com>
2022-04-10 15:53:18 +0900

    [rust][155] add unit test

--- 155.min-stack.rs
+++ 155.min-stack.rs
@@ -4,34 +4,63 @@
  * [155] Min Stack
  */
 
+struct Solution;
+
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+
+#[macro_export]
+macro_rules! list_node {
+    ( $x:expr,$( $y:expr ),* ) => {
+        {
+            let mut head = Some(Box::new(ListNode::new($x)));
+            let mut tail = &mut head;
+            $(
+                let mut node = Some(Box::new(ListNode::new($y)));
+                tail.as_mut().unwrap().next = node;
+                tail = &mut tail.as_mut().unwrap().next;
+            )*
+            head
+        }
+    };
+}
+
 // @lc code=start
 struct MinStack {
     stack: Vec<i32>,
 }
 
-
-/** 
+/**
  * `&self` means the method takes an immutable reference.
  * If you need a mutable reference, change it to `&mut self` instead.
  */
 impl MinStack {
-
     fn new() -> Self {
-        MinStack{stack: vec![]}
+        MinStack { stack: vec![] }
     }
-    
+
     fn push(&mut self, val: i32) {
         self.stack.push(val)
     }
-    
+
     fn pop(&mut self) {
-        self.stack.pop();        
+        self.stack.pop();
     }
-    
+
     fn top(&self) -> i32 {
-        self.stack[self.stack.len() - 1]        
+        self.stack[self.stack.len() - 1]
     }
-    
+
     fn get_min(&self) -> i32 {
         let mut min_val = std::i32::MAX;
         for num in &self.stack {
@@ -51,3 +80,14 @@ impl MinStack {
  */
 // @lc code=end
 
+#[test]
+fn test1() {
+    let mut stack = MinStack::new();
+    stack.push(-2);
+    stack.push(0);
+    stack.push(-3);
+    assert_eq!(stack.get_min(), -3);
+    stack.pop();
+    assert_eq!(stack.top(), 0);
+    assert_eq!(stack.get_min(), -2);
+}
