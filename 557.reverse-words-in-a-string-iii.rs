3013e2e319d7abd080645e0996895a49e9ce67c5
Kondo Takeo <takeoverjp@gmail.com>
2022-06-28 23:07:43 +0900

    [rust][557] refactoring

--- 557.reverse-words-in-a-string-iii.rs
+++ 557.reverse-words-in-a-string-iii.rs
@@ -59,32 +59,13 @@ macro_rules! list_node {
 // @lc code=start
 impl Solution {
     pub fn reverse_words(s: String) -> String {
-        let len = s.len();
-        let s = s.chars().collect::<Vec<char>>();
-        let mut ret = s.clone();
-        let mut start = 0;
-        while start < len {
-            let mut end = start;
-            eprintln!("start = {}, len = {}", start, len);
-            for i in start..=len {
-                if i == len {
-                    end = len;
-                    break;
-                }
-                if s[i] == ' ' {
-                    end = i;
-                    break;
-                }
-            }
-
-            for i in start..end {
-                ret[i] = s[(end - 1) - (i - start)];
-            }
-
-            start = end + 1;
-        }
-
-        ret.iter().collect()
+        s.chars()
+            .rev()
+            .collect::<String>()
+            .split_whitespace()
+            .rev()
+            .collect::<Vec<&str>>()
+            .join(" ")
     }
 }
 // @lc code=end
