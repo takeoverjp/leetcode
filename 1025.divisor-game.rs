fafc0573644efbb89fa3a6917956377c180ad3c7
Kondo Takeo <takeoverjp@gmail.com>
2022-09-12 22:56:53 +0900

    [rust][1025] Divisor Game

---
+++ 1025.divisor-game.rs
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode id=1025 lang=rust
+ *
+ * [1025] Divisor Game
+ */
+
+struct Solution;
+
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<std::rc::Rc<std::cell::RefCell<TreeNode>>>,
+    pub right: Option<std::rc::Rc<std::cell::RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! list_node {
+    () => {
+        None
+    };
+    ( $x:expr,$( $y:expr ),* ) => {
+        {
+            let mut head = Some(Box::new(ListNode::new($x)));
+            let mut tail = &mut head;
+            $(
+                let node = Some(Box::new(ListNode::new($y)));
+                tail.as_mut().unwrap().next = node;
+                tail = &mut tail.as_mut().unwrap().next;
+            )*
+            head
+        }
+    };
+}
+
+// @lc code=start
+impl Solution {
+    pub fn divisor_game(n: i32) -> bool {
+        n % 2 == 0
+    }
+}
+// @lc code=end
+
+#[test]
+fn test1() {
+    assert!(Solution::divisor_game(2));
+}
+
+#[test]
+fn test2() {
+    assert!(!Solution::divisor_game(3));
+}
