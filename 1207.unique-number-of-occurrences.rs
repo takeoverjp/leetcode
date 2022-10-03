4fe6bf31e54132220e1641d58c0f02f042e49795
Kondo Takeo <takeoverjp@gmail.com>
2022-10-03 23:36:34 +0900

    [rust][1207] Unique Number of Occurrences

---
+++ 1207.unique-number-of-occurrences.rs
@@ -0,0 +1,91 @@
+/*
+ * @lc app=leetcode id=1207 lang=rust
+ *
+ * [1207] Unique Number of Occurrences
+ */
+
+struct Solution;
+
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<std::rc::Rc<std::cell::RefCell<TreeNode>>>,
+    pub right: Option<std::rc::Rc<std::cell::RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! list_node {
+    () => {
+        None
+    };
+    ( $x:expr,$( $y:expr ),* ) => {
+        {
+            let mut head = Some(Box::new(ListNode::new($x)));
+            let mut tail = &mut head;
+            $(
+                let node = Some(Box::new(ListNode::new($y)));
+                tail.as_mut().unwrap().next = node;
+                tail = &mut tail.as_mut().unwrap().next;
+            )*
+            head
+        }
+    };
+}
+
+// @lc code=start
+impl Solution {
+    pub fn unique_occurrences(arr: Vec<i32>) -> bool {
+        let mut count = std::collections::HashMap::<i32, i32>::new();
+        for a in arr {
+            let a = count.entry(a).or_insert(0);
+            *a += 1;
+        }
+        count.len()
+            == (count
+                .values()
+                .collect::<std::collections::HashSet<&i32>>()
+                .len())
+    }
+}
+// @lc code=end
+
+#[test]
+fn test1() {
+    assert!(Solution::unique_occurrences(vec![1, 2, 2, 1, 1, 3]));
+}
+
+#[test]
+fn test2() {
+    assert!(!Solution::unique_occurrences(vec![1, 2]));
+}
+
+#[test]
+fn test3() {
+    assert!(Solution::unique_occurrences(vec![
+        -3, 0, 1, -3, 1, 1, 1, -3, 10, 0
+    ]));
+}
