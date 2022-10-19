c7da6211e48bf13d441e2305852d61a3b60608b8
Kondo Takeo <takeoverjp@gmail.com>
2022-10-19 21:42:31 +0900

    [rust][1313] Decompress Run-Length Encoded List

---
+++ 1313.decompress-run-length-encoded-list.rs
@@ -0,0 +1,89 @@
+/*
+ * @lc app=leetcode id=1313 lang=rust
+ *
+ * [1313] Decompress Run-Length Encoded List
+ */
+
+struct Solution;
+
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<std::rc::Rc<std::cell::RefCell<TreeNode>>>,
+    pub right: Option<std::rc::Rc<std::cell::RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! list_node {
+    () => {
+        None
+    };
+    ( $x:expr,$( $y:expr ),* ) => {
+        {
+            let mut head = Some(Box::new(ListNode::new($x)));
+            let mut tail = &mut head;
+            $(
+                let node = Some(Box::new(ListNode::new($y)));
+                tail.as_mut().unwrap().next = node;
+                tail = &mut tail.as_mut().unwrap().next;
+            )*
+            head
+        }
+    };
+}
+
+// @lc code=start
+impl Solution {
+    pub fn decompress_rl_elist(nums: Vec<i32>) -> Vec<i32> {
+        let mut ret = vec![];
+        let mut i = 0;
+        while i < nums.len() {
+            for _ in 0..nums[i as usize] {
+                ret.push(nums[(i + 1) as usize])
+            }
+            i += 2;
+        }
+        ret
+    }
+}
+// @lc code=end
+
+#[test]
+fn test1() {
+    assert_eq!(
+        Solution::decompress_rl_elist(vec![1, 2, 3, 4]),
+        vec![2, 4, 4, 4]
+    );
+}
+
+#[test]
+fn test2() {
+    assert_eq!(
+        Solution::decompress_rl_elist(vec![1, 1, 2, 3]),
+        vec![1, 3, 3]
+    );
+}
