ee97af280ca185d60b197e7d3a1576991a4e2f17
Kondo Takeo <takeoverjp@gmail.com>
2022-10-25 22:26:54 +0900

    [rust][1342] Number of Steps to Reduce a Number to Zero

--- 1342.number-of-steps-to-reduce-a-number-to-zero.rs
+++ 1342.number-of-steps-to-reduce-a-number-to-zero.rs
@@ -4,20 +4,75 @@
  * [1342] Number of Steps to Reduce a Number to Zero
  */
 
+struct Solution;
+
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<std::rc::Rc<std::cell::RefCell<TreeNode>>>,
+    pub right: Option<std::rc::Rc<std::cell::RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! list_node {
+    () => {
+        None
+    };
+    ( $x:expr,$( $y:expr ),* ) => {
+        {
+            let mut head = Some(Box::new(ListNode::new($x)));
+            let mut tail = &mut head;
+            $(
+                let node = Some(Box::new(ListNode::new($y)));
+                tail.as_mut().unwrap().next = node;
+                tail = &mut tail.as_mut().unwrap().next;
+            )*
+            head
+        }
+    };
+}
+
 // @lc code=start
 impl Solution {
     pub fn number_of_steps(num: i32) -> i32 {
-        let mut ret = 0;
-        let mut num = num;
-        while (num > 0) {
-            if num % 2 == 1 {
-                num -= 1;
-            } else {
-                num /= 2;
-            }
-            ret += 1;
+        if num == 0 {
+            return 0;
         }
-        ret
+        (num.count_ones() + 31 - num.leading_zeros()) as i32
     }
 }
 // @lc code=end
+
+#[test]
+fn test1() {
+    assert_eq!(Solution::number_of_steps(14), 6);
+}
+
+#[test]
+fn test2() {
+    assert_eq!(Solution::number_of_steps(8), 4);
+}
